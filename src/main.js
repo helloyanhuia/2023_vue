ad363194841af26ca1c984f51125e7b674cf4882
呵呵 <317201396@qq.com>
2023-04-14 23:28:34 +0800

    VUE组件化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 指定不显示非生产环境模式的提示
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   render: (h) => h(App),
-}).$mount('#app')
+})
